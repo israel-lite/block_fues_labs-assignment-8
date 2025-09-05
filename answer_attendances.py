f41deb00c096c40e30d36d3c42606f4d2062bff3
israeliiii <israelite1804@gmail.com>
2025-09-05 01:05:56 -0700

    update: update answer_attendance

--- answer_attendances.py
+++ answer_attendances.py
@@ -1,22 +1,46 @@
 import datetime
 
-# today = datetime.date.today()
-# print(f"Today's date is: {today}")
-
 class AttendanceRegister:
     def __init__(self):
         self.attendance = {}
 
     def register_student(self, name):
         if name not in self.attendance:
-            self.attendance[name] = {
-                present_days: [],
-                absent_days: []
-            }
+            self.attendance[name] = {"present_days": [], "absent_days": []}
         else:
-            return "Student already registered"
+            print("Student already registered")
+
+    
+    def mark_present(self, name):
+        today = str(datetime.date.today())
+        if name in self.attendance:
+            if today not in self.attendance[name]["present_days"]:
+                self.attendance[name]["present_days"].append(today)
+                if today in self.attendance[name]["absent_days"]:
+                    self.attendance[name]["absent_days"].remove(today)
+            else:
+                print(f"{name} is already marked present today.")
+        else:
+            print(f"{name} is not registered.")
         
+    def mark_absent(self, name):
+        today = str(datetime.date.today())
+        if name in self.attendance:
+            if today not in self.attendance[name]["absent_days"]:
+                self.attendance[name]["absent_days"].append(today)
+                if today in self.attendance[name]["present_days"]:
+                    self.attendance[name]["present_days"].remove(today)
+            else:
+                print(f"{name} is already marked absent today.")
+        else:
+            print(f"{name} is not registered.")
 
-student1 = AttendanceRegister()
+record = AttendanceRegister()
 
-print(student1)
+record.register_student("John")
+
+record.register_student("Mary")
+print(record.attendance)
+
+record.mark_absent("John")
+print(record.attendance)
