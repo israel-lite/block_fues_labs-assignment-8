bacaf94fd604bf193669354cb67dba01024009d8
israeliiii <israelite1804@gmail.com>
2025-09-06 01:44:27 -0700

    add:added my bankaccount

--- bank_answer.py
+++ bank_answer.py
@@ -3,19 +3,22 @@ class BankAccount:
         self.owner = owner
         self.balance = starting_balance
 
-    def deposit(self, amount):
+#login = str(input("Enter login to login\n"))
+#create_account = set(input("reate an occount!\n"))
+                         
+def deposit(self, amount):
         if amount > 0:
             self.balance += amount
             return True
         return False
 
-    def withdraw(self, amount):
+def withdraw(self, amount):
         if 0 < amount <= self.balance:
             self.balance -= amount
             return True
         return False
 
-    def get_balance(self, ):
+def get_balance(self, ):
         return self.balance
 #example usage
 acc = BankAccount("Alice", 1000)
@@ -26,3 +29,29 @@ print(acc.get_balance())
 for item in dir(acc):
     if not item.startswith("__"):
         print(item)
+
+import random
+def __init__(self, account_name, account_number, balance, promo):
+     account_name = random
+     account_number = random.randint
+     balance = 5000
+     promo = random
+Alice = random.randint = BankAccount
+Alice = random.randint = Alice
+
+#def promo(hasproma=False):
+     #class BankAccount: 
+BankAccount = (elf, name, amount, haspromo=False)
+promo_prize = 2_000
+def __init__(self, name, amount, haspromo=False):
+    if haspromo:
+        amount += self.promo_prize
+
+        self.account_name = name
+        self.account_balance = amount
+        self.account_number = random.randint(100, 900)
+ab = BankAccount("ab", 1000, True)
+jc = BankAccount("Jc", 200)
+
+print(ab)
+print(((((((jc)))))))
